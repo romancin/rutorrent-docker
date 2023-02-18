f4bd821994d0d75df18a7f3ca173e6a809071c9a
Jolly-Pirate <4613678+Jolly-Pirate@users.noreply.github.com>
2023-02-18 13:52:17 -0500

    build.sh and .env
    Build script with override variables stored in .env. This provides a more flexible setup without having to update Dockerfile with frequent versions.

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+if [[ -f .env ]]; then
+    source .env
+else
+    echo "Missing .env, copying example .env from template. Edit it before running build.sh again."
+    cp .env-example .env
+    exit
+fi
+
+echo "Building ruTorrent $RUTORRENT_VER"
+sleep 2
+
+docker build --no-cache \
+    --build-arg BASEIMAGE_VERSION=$BASEIMAGE_VERSION \
+    --build-arg RTORRENT_VER=$RTORRENT_VER \
+    --build-arg LIBTORRENT_VER=$LIBTORRENT_VER \
+    --build-arg MEDIAINF_VER=$MEDIAINF_VER \
+    --build-arg CURL_VER=$CURL_VER \
+    --build-arg MAXMIND_LICENSE_KEY=$MAXMIND_LICENSE_KEY \
+    --build-arg TARGETARCH=amd64 \
+    --build-arg RUTORRENT_VER=$RUTORRENT_VER \
+    --network=host \
+    --tag "rutorrent:latest" \
+    --tag "rutorrent:$RUTORRENT_VER" \
+    -f Dockerfile .
